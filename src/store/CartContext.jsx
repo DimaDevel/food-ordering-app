385d4b6af1c52799332c1ff11427d5fd1b128523
DimaDevel <dima.ivanov.dev@gmail.com>
2025-04-17 20:21:39 +0200

    Added context in the MealItem component.

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -8,8 +8,7 @@ const CartContext = createContext({
 
 function cartReducer(state, action) {
   switch (action.type) {
-    case "ADD_ITEM":
-      // ... update the state to add a meal item
+    case "ADD_ITEM": {
       const existingCartItemIndex = state.items.findIndex(
         (item) => item.id === action.item.id,
       );
@@ -27,18 +26,55 @@ function cartReducer(state, action) {
       }
 
       return { ...state, items: updatedItems };
-    case "REMOVE_ITEM":
-      // ... remove an item from the state
-      break;
+    }
+
+    case "REMOVE_ITEM": {
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.id,
+      );
+
+      const existingCartItem = state.items[existingCartItemIndex];
+
+      const updatedItems = [...state.items];
+      if (existingCartItem.quantity === 1) {
+        updatedItems.splice(existingCartItemIndex, 1);
+      } else {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity - 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItems;
+      }
+
+      return { ...state, items: updatedItems };
+    }
   }
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, {
     items: [],
   });
-  return <CartContextProvider>{children}</CartContextProvider>;
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  console.log("cartContext", cartContext);
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;
