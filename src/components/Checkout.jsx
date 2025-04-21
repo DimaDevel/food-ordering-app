110b1c1a3857636b4dc73cebaca24fc4e30aec78
DimaDevel <dima.ivanov.dev@gmail.com>
2025-04-21 14:20:29 +0200

    Added Checkout Component.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,1 +1,45 @@
-export default function Checkout() {}
+import Modal from "./UI/Modal.jsx";
+import { useContext } from "react";
+import CartContext from "../store/CartContext.jsx";
+import { currencyFormatter } from "../util/formatting.js";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0,
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+        <Input lable="Full Name" type="text" id="full-name" />
+        <Input lable="E-Mail Address " type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+
+        <p className="modal-actions">
+          <Button onClick={handleClose} type="button" textOnly>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
+    </Modal>
+  );
+}
